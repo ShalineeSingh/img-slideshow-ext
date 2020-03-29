9e299eaa9d81807e65f4a54520db707e6c9aa809
Shalinee Singh <shalinee.singh@coviam.com>
2020-03-29 18:39:53 +0530

    pinterest bucket change logic

--- script/getPageSource.js
+++ script/getPageSource.js
@@ -1,14 +1,17 @@
-function parseImagesFromDOM(document_root) {
+function parseImagesFromDOM(document_root, source_url) {
   var image_array = [];
   var images = document_root.getElementsByTagName('img');
   for (var element of images) {
     if (element.naturalHeight > 100 && element.naturalWidth > 100)
-      image_array.push(element.src);
+      if (source_url.includes('pinterest')) {
+        element.src = element.src.replace('236x', '564x');
+      }
+    image_array.push(element.src);
   }
   return image_array;
 }
 
 chrome.runtime.sendMessage({
   action: 'getSource',
-  source: parseImagesFromDOM(document)
+  source: parseImagesFromDOM(document, location.href)
 });
--- script/slideshow.js
+++ script/slideshow.js
@@ -3,7 +3,6 @@
     function (message, sender, sendResponse) {
       var image_array = message.images;
       for (var i = 0; i < image_array.length; i++) {
-        console.log($('.carousel-inner'));
         $('<div class="item"><img src="' + image_array[i] + '"><div class="carousel-caption"></div>   </div>').appendTo('.carousel-inner');
         $('<li data-target="#imageCarousel" data-slide-to="' + i + '"></li>').appendTo('.carousel-indicators');
       }
