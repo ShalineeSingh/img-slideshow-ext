f64874c7dd14ae73634f4c70b83950e9e9d7d2be
Shalinee Singh <shalinee.singh@coviam.com>
2020-03-29 18:28:39 +0530

    added image size filter

--- script/getPageSource.js
+++ script/getPageSource.js
@@ -2,7 +2,8 @@ function parseImagesFromDOM(document_root) {
   var image_array = [];
   var images = document_root.getElementsByTagName('img');
   for (var element of images) {
-    image_array.push(element.src);
+    if (element.naturalHeight > 100 && element.naturalWidth > 100)
+      image_array.push(element.src);
   }
   return image_array;
 }
--- script/slideshow.js
+++ script/slideshow.js
@@ -5,10 +5,29 @@
       for (var i = 0; i < image_array.length; i++) {
         console.log($('.carousel-inner'));
         $('<div class="item"><img src="' + image_array[i] + '"><div class="carousel-caption"></div>   </div>').appendTo('.carousel-inner');
-        $('<li data-target="#carousel-example-generic" data-slide-to="' + i + '"></li>').appendTo('.carousel-indicators');
+        $('<li data-target="#imageCarousel" data-slide-to="' + i + '"></li>').appendTo('.carousel-indicators');
       }
       $('.item').first().addClass('active');
       $('.carousel-indicators > li').first().addClass('active');
-      $('#carousel-example-generic').carousel();
+      $('#imageCarousel').carousel({
+        interval: 1000,
+        pause: "false"
+      });
     });
+
+  $('#playButton').click(function () {
+    $('#imageCarousel').carousel('cycle');
+  });
+  $('#pauseButton').click(function () {
+    $('#imageCarousel').carousel('pause');
+  });
+  $('#downloadButton').click(function () {
+    var src = $('.active').find('img').attr('src');
+    var link = document.createElement('a');
+    link.href = src;
+    link.download = 'image.jpg';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  });
 })(jQuery);
